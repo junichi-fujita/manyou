b8b7d6797e89b5baf8a257081015a10afd66ddf8
junichi-fujita <fujita0909j@gmail.com>
2020-03-20 18:03:25 +0900

    taskモデルのテストを記述

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it ""
+  example "名前を空白にしてはいけない。" do
+    task = Task.new(name: nil, description: "aaa")
+    task.valid?
+    expect(task.errors.messages[:name]).to include("can't be blank") 
+  end
+
+  example "詳しい記述を空白にしてはいけない。" do
+    task = Task.new(name: "aaa", description: nil)
+    task.valid?
+    expect(task.errors.messages[:description]).to include("can't be blank")
+  end
+  
+  example "詳しい記述は２５５文字以下でなければならない。" do
+    task = Task.new(name: "aaa", description: "a" * 256)
+    task.valid?
+    expect(task.errors.messages[:description]).to include("is too long (maximum is 255 characters)")
+  end
 end
