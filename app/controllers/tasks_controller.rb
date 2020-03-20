1e3bcb6e8a66f2f770d13506184e9c14232e5e0b
junichi-fujita <fujita0909j@gmail.com>
2020-03-20 13:45:35 +0900

    view及びcontrollerにおいて、taskのcrud処理を作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.order(created_at: :desc)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "「#{@task.name}」を作成しました。"
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @task.assign_attributes(task_params)
+    if @task.save
+      redirect_to @task, notice: "「#{@task.name}」を更新しました。"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :description,
+      :created_at,
+      :updated_at,
+    )
+  end
+end
