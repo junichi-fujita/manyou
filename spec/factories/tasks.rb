5af5021a1d196619bed6759adeb49031774a9e49
junichi-fujita <fujita0909j@gmail.com>
2020-03-20 19:30:33 +0900

    systemspecのテストを作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :task do
-    
+    sequence(:name) { |n| "test_name#{n+1}" }
+    sequence(:description) { |n| "test_description#{n+1}" }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -12,10 +12,34 @@ RSpec.describe Task, type: :model do
     task.valid?
     expect(task.errors.messages[:description]).to include("can't be blank")
   end
-  
+
   example "詳しい記述は２５５文字以下でなければならない。" do
     task = Task.new(name: "aaa", description: "a" * 256)
     task.valid?
     expect(task.errors.messages[:description]).to include("is too long (maximum is 255 characters)")
   end
+
+  before do
+    @task = build(:task)
+  end
+
+  example "nameとdescriptionの両方が記述されていればok" do
+    expect(@task.valid?).to eq(true)
+  end
+
+  example "nameが空だとNG" do
+    @task.name = ""
+    expect(@task.valid?).to eq(false)
+  end
+
+  example "descriptionが空だとNG" do
+    @task.description = ""
+    expect(@task.valid?).to eq(false)
+  end
+
+  example "descriptionが２５６文字以上ならNG" do
+    @task.description = "a" * 256
+    expect(@task.valid?).to eq(false)
+  end
+
 end
