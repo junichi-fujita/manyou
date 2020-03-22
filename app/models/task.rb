5670085c02f2b54e0a0ca652bf0813925cc264b7
junichi-fujita <fujita0909j@gmail.com>
2020-03-22 13:36:04 +0900

    scopeを使用し、検索条件を設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 255 }
 
+  scope :search_subject, -> (name_arr) { where("name LIKE ?", name_arr.map{|n|"%#{n}%"})}
+
   STATUS_VALUES = %w(yet started done)
   class << self
     def status_text(status)
@@ -12,24 +14,18 @@ class Task < ApplicationRecord
       STATUS_VALUES.map { |status| [status_text(status), status]}
     end
 
-    # scope :search_subject, ->(query) { where("name LIKE ?", "%#{query}%")}
-    # scope :search_status, ->(query) { where("status = ?", "#{query}")}
-
     def search(query)
-      q_arr = query.split(/[[:blank:]]+/)
-      st_val = { "未着手" => "yet", "着手済" => "started", "完了" => "done" }
+      query_arr = query.split(/[[:blank:]]+/)
+      status_hash = { "未着手" => "yet", "着手済" => "started", "完了" => "done" }
 
-      st_arr = q_arr.select { |n| n =~ /^未着手$|^着手済$|^完了$/}.map { |n| st_val[n] }
-      name_arr = q_arr.reject { |n| n =~ /^未着手$|^着手済$|^完了$/}
+      status_arr = query_arr.select { |n| n =~ /^未着手$|^着手済$|^完了$/}.map { |n| status_hash[n] }
+      name_arr = query_arr.reject { |n| n =~ /^未着手$|^着手済$|^完了$/}
       rel = order("id")
 
       if query.present?
         rel = rel.where(
-          status: st_arr
-          ).or(rel.where(
-            "name LIKE ?",
-            name_arr.map{|n|"%#{n}%"}
-          ))
+          status: status_arr
+          ).or(rel.search_subject(name_arr))
       end
       rel
     end
