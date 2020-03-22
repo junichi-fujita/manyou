a13fd871863879a50a7065b2e266e14a81c6e037
junichi-fujita <fujita0909j@gmail.com>
2020-03-22 09:38:27 +0900

    検索フォームの修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -12,17 +12,24 @@ class Task < ApplicationRecord
       STATUS_VALUES.map { |status| [status_text(status), status]}
     end
 
+    # scope :search_subject, ->(query) { where("name LIKE ?", "%#{query}%")}
+    # scope :search_status, ->(query) { where("status = ?", "#{query}")}
+
     def search(query)
+      q_arr = query.split(/[[:blank:]]+/)
       st_val = { "未着手" => "yet", "着手済" => "started", "完了" => "done" }
-      if query =~ /^未着手$|^着手済$|^完了$/
-        query = st_val[query]
-      end
+
+      st_arr = q_arr.select { |n| n =~ /^未着手$|^着手済$|^完了$/}.map { |n| st_val[n] }
+      name_arr = q_arr.reject { |n| n =~ /^未着手$|^着手済$|^完了$/}
       rel = order("id")
+      
       if query.present?
         rel = rel.where(
-          "name LIKE ? OR status = ?",
-          "%#{query}%", "#{query}"
-          )
+          status: st_arr
+          ).or(rel.where(
+            "name LIKE ?",
+            name_arr.map{|n|"%#{n}%"}
+          ))
       end
       rel
     end
