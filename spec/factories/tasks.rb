f14c2d2c7fcb28f4dde969a592d7aae9816b5094
junichi-fujita <fujita0909j@gmail.com>
2020-03-22 17:14:07 +0900

    優先順位のソートボタンのテストを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,10 +3,12 @@ FactoryBot.define do
     name { "name1" }
     description { "description1" }
     end_date { Time.current}
+    priority { "low" }
   end
   factory :second_task, class: Task do
     name { "name2" }
     description { "description2" }
     end_date { Time.current + 1.days}
+    priority {"high"}
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
+require 'selenium-webdriver'
+# wait = Selenium::WebDriver::Wait.new(:timeout => 100)
+
 RSpec.describe "タスク管理機能", type: :system do
   # before do
   #   @task = Task.create!(name: "aaa", description: "bbb")
@@ -7,12 +10,12 @@ RSpec.describe "タスク管理機能", type: :system do
   before do
     @task = create(:task)
     @second_task = create(:second_task)
+    visit root_path
   end
 
   describe "タスク一覧画面" do
     context "タスクを作成した場合" do
       it "作成済のタスクが表示されること" do
-        visit root_path
         expect(page).to have_content "name1"
         expect(page).to have_content "name2"
         # expect(page).to have_content "ccc"
@@ -21,28 +24,47 @@ RSpec.describe "タスク管理機能", type: :system do
     context "複数のタスクを作成した場合" do
       it "タスクが作成日時の降順に並んでいること" do
         # new_task = create(:task, name: "new_task", description: "new_description")
-        visit root_path
+        # visit root_path
         task_list = all("tbody tr")
         # binding.pry
         expect(task_list[0]).to have_content "name2"
         expect(task_list[1]).to have_content "name1"
       end
     end
-    context "昇順ボタンを押した場合" do
+    context "終了期限の昇順ボタンを押した場合" do
       it "タスクが終了期限の昇順に並んでいること" do
-        visit root_path
-        click_link "▲"
+        # visit root_path
+        click_on "end_asc"
         sort_list = all(".sort")
         # binding.pry
         expect(sort_list[0]).to have_content "name1"
         expect(sort_list[1]).to have_content "name2"
       end
     end
-    context "降順ボタンを押した場合" do
+    context "終了期限の降順ボタンを押した場合" do
       it "タスクが終了期限の降順に並んでいること" do
-        visit root_path
-        click_link "▲"
-        click_link "▼"
+        # visit root_path
+        click_on "end_desc"
+        sort_list = all(".sort")
+        expect(sort_list[0]).to have_content "name2"
+        expect(sort_list[1]).to have_content "name1"
+      end
+    end
+
+    context "優先順位の昇順ボタンを押した場合" do
+      it "優先順位が優先度低い順に並んでいること" do
+        # visit root_path
+        # wait.until { driver.find_element(:id, "pri_asc").displayed? }
+        click_on "pri_asc"
+        sort_list = all(".sort")
+        expect(sort_list[0]).to have_content "name1"
+        expect(sort_list[1]).to have_content "name2"
+      end
+    end
+
+    context "優先順位の降順ボタンを押した場合" do
+      it "優先順位が優先度高い順に並んでいること" do
+        click_on "pri_desc"
         sort_list = all(".sort")
         expect(sort_list[0]).to have_content "name2"
         expect(sort_list[1]).to have_content "name1"
