4f172bdea0d55ddce870b76eb2611e92aebaebe6
junichi-fujita <fujita0909j@gmail.com>
2020-03-24 23:41:08 +0900

    :tada: [add]refs #4 seedデータにuserを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,21 @@
 if Rails.env == "development"
+  status_arr = %w(yet started done)
   50.times do |i|
     Task.create!(
       name: "test#{i + 1}",
       description: "test#{i + 1}だよ！", 
       end_date: "2020-03-22 08:26:00",
-      priority: i % 3
+      priority: i % 3,
+      status: status_arr[i % 3]
+    )
+  end
+
+  10.times do |i|
+    User.create!(
+      name: "user#{i + 1}",
+      email: "user#{i + 1}@test.com",
+      password: "aaa",
+      password_confirmation: "aaa"
     )
   end
 end
