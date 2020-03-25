88b3171d876c98f0659c043935ffe444cd9af05c
junichi-fujita <fujita0909j@gmail.com>
2020-03-25 22:51:55 +0900

    [fix]refs #4 usercontrollerのエラーを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,26 +4,26 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(:id)
+    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user)
+      redirect_to user_path(@user), notice: "「#{@user.name}」を登録しました。"
     else
       render "new"
     end
+  end
 
-    private
+  private
 
-    def user_params
-      params.require(:user).permit(
-        :name,
-        :email,
-        :password,
-        :password_confirmation,
-      )
-    end
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation,
+    )
   end
 end
