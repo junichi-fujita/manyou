35b5d0e811bc87c9acccabdfe29cd384642d91f2
junichi-fujita <fujita0909j@gmail.com>
2020-03-26 22:58:20 +0900

    :tada: [add]refs #4 loginしてない状態でtask画面に遷移した場合、ユーザー登録画面に遷移する。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  class LoginRequired < StandardError; end
+  class Forbidden < StandardError; end
+
   private 
-    def current_user
-      User.find_by(id: session[:user_id]) if session[:user_id]
+
+  def current_user
+    User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    unless current_user
+      redirect_to new_user_path
     end
+  end
 end
