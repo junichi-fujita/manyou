d55516d265b4006f606a8ab0376b60b64861d65a
junichi-fujita <fujita0909j@gmail.com>
2020-03-28 09:57:38 +0900

    [add]refs #4 管理者画面のルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw d  namespace :admin do
+    root "top#index"
+    resources :users, except: [:index, :show]
+  end
   root to: "tasks#index"
   resources :tasks, except: :index do
     get "search", on: :collection
