e5b7f1e36cd755edca116e9e5e2ce88d5baa79d7
junichi-fujita <fujita0909j@gmail.com>
2020-04-05 16:14:15 +0900

    [clean]refs #4 admin_login_requiredメソッドをリファクタリング

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -4,9 +4,9 @@ class Admin::Base < ApplicationController
   private
 
   def admin_login_required
-    if logged_in? == false
+    if !logged_in? 
       redirect_to new_user_path
-    elsif logged_in_as_admin? == false
+    elsif !logged_in_as_admin?
       redirect_to root_path
     end
   end
