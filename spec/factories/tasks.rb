de8d6b82aa957c18865e9a1a38dced17ad2f3784
junichi-fujita <fujita0909j@gmail.com>
2020-04-05 22:53:23 +0900

    [clean]refs #4 enumで設定した設定した値をテストにおいてsymbolに変更

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,17 +2,17 @@ FactoryBot.define do
   factory :task do
     name { "name1" }
     description { "description1" }
-    end_date { Time.current}
+    end_date { Time.current }
     status { "yet" }
-    priority { "low" }
+    priority { :low }
     user_id { 1 }
   end
   factory :second_task, class: Task do
     name { "name2" }
     description { "description2" }
-    end_date { Time.current + 1.days}
-    status { "done"}
-    priority {"high"}
+    end_date { Time.current + 1.days }
+    status { "done" }
+    priority { :high }
     user_id { 2 }
   end
 end
