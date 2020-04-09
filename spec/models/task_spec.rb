073de764c6b2d5d40e67b8f54d71277eee12d730
junichi-fujita <fujita0909j@gmail.com>
2020-04-09 22:54:39 +0900

    [clean]refs #5 task_specの検索結果の変数の名前を変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -29,19 +29,19 @@ RSpec.describe Task, type: :model do
     end
 
     example "名前検索欄でe1を検索するとname1が表示される" do
-      name1 = Task.search_name("e1")
-      expect(name1[0].name).to eq(@test1.name)
+      result1 = Task.search_name("e1")
+      expect(result1[0].name).to eq(@test1.name)
     end
   
     example "状態検索欄でdoneを検索するとname2が表示される" do
-      name2 = Task.search_status("done")
-      expect(name2[0].name).to eq(@test2.name)
+      result2 = Task.search_status("done")
+      expect(result2[0].name).to eq(@test2.name)
     end
   
     example "名前検索欄でe1,状態検索欄でdoneを検索するとname1とname2が表示される" do
-      name3 = Task.search_double("e1","done")
-      expect(name3[0].name).to eq(@test1.name)
-      expect(name3[1].name).to eq(@test2.name)
+      result3 = Task.search_double("e1","done")
+      expect(result3[0].name).to eq(@test1.name)
+      expect(result3[1].name).to eq(@test2.name)
     end
   end
 end
