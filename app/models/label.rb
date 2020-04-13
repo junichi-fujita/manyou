38412232ac27e162e1649e57dc027c2b79fb417d
junichi-fujita <fujita0909j@gmail.com>
2020-04-13 15:25:39 +0900

    [add]refs #5 labelモデルにバリデーションを追加

--- app/models/label.rb
+++ app/models/label.rb
@@ -2,4 +2,6 @@ class Label < ApplicationRecord
   has_many :labelings, dependent: :destroy
   has_many :tasks, through: :labelings
   belongs_to :user, optional: true
+
+  validates name:, presence: true, length: { in: 1..20 }
 end
