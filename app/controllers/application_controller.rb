6b791898ddc2184e4e30a020e695abecf4b0a7cd
junichi-fujita <fujita0909j@gmail.com>
2020-04-13 16:01:46 +0900

    [change]refs #5 管理者はすべてのタスクを、カレントユーザーは自分のタスクのみを変更・削除できるように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :logged_in_as_admin?
   class LoginRequired < StandardError; end
   class Forbidden < StandardError; end
 
