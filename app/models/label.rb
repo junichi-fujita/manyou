1ea492e471bcc1759f8b58020bb7a1fc31e1eaac
junichi-fujita <fujita0909j@gmail.com>
2020-04-13 15:27:47 +0900

    [clean]refs #5 labelモデルのバリデーションエラーを修正

--- app/models/label.rb
+++ app/models/label.rb
@@ -3,5 +3,5 @@ class Label < ApplicationRecord
   has_many :tasks, through: :labelings
   belongs_to :user, optional: true
 
-  validates name:, presence: true, length: { in: 1..20 }
+  validates :name, presence: true, length: { in: 1..20 }
 end
