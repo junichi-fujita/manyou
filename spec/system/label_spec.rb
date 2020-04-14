44a9b7c0a6374d9c4bb96dc542c82d8de85b9859
junichi-fujita <fujita0909j@gmail.com>
2020-04-15 07:27:03 +0900

    [add]refs #5 label_specのテストを追加

---
+++ spec/system/label_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+require 'selenium-webdriver'
+
+RSpec.describe "ラベル管理機能", type: :system do
+  let!(:user) { create(:user) }
+  let!(:label) { create(:label, user: user) }
+  let(:another_label) { create(:another_label, user: user) }
+  let!(:labels) { create_list(:label, 6) }
+
+  def login
+    visit new_user_path
+    fill_in "t_email", with: "sample1@example.com"
+    fill_in "t_password", with: "aaa"
+    click_on "ログイン"
+  end
+
+  describe "ラベルの作成画面" do
+    before do
+      login
+    end
+    it "ラベル作成画面で、ラベルを入力するとタスクの新規画面にラベルが表示される" do
+      visit new_label_path
+      fill_in "label_name", with: "c++"
+      click_on "登録する"
+      expect(page).to have_content("c++")
+    end
+    it "タスク新規登録画面で、ラベルを削除するとラベルが消える" do
+      visit new_task_path
+      label1 = create(:label, name: "test1", user_id: user.id)
+      click_on "削除する"
+      expect(page).to have_content("ラベルを削除しました。")
+    end
+  end
+end
